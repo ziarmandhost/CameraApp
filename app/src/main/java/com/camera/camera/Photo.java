0018220dca310bd550488a4bd7d3dc76041ac124
Dmitriy Chiruk <dcfild@gmail.com>
2019-03-20 00:48:04 +0200

    3 commit

--- app/src/main/java/com/camera/camera/Photo.java
+++ app/src/main/java/com/camera/camera/Photo.java
@@ -1,10 +1,15 @@
 package com.camera.camera;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -13,9 +18,20 @@ import com.camera.camera.ui.base.BaseFragment;
 public class Photo extends BaseFragment {
 
     private PhotoViewModel mViewModel;
+    private SurfaceView surfaceView;
+    private SurfaceHolder mHolder;
 
     public static Photo newInstance() {
         return new Photo();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(PhotoViewModel.class);
+        surfaceView = getView().findViewById(R.id.surfaceView);
+        checkCameraHardware(getContext());
+        getCameraInstance();
     }
 
     @Override
@@ -25,14 +41,28 @@ public class Photo extends BaseFragment {
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(PhotoViewModel.class);
-        // TODO: Use the ViewModel
-    }
-
-    @Override
     public String getName() {
         return "Фото";
+    }
+
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open();// attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 }
