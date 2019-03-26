4cd91da1770b4da20a2d440e894dd33b3a3afff4
Dmitriy Chiruk <dcfild@gmail.com>
2019-03-26 21:08:01 +0200

    6 commit

--- app/src/main/java/com/camera/camera/MainActivity.java
+++ app/src/main/java/com/camera/camera/MainActivity.java
@@ -1,12 +1,21 @@
 package com.camera.camera;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.camera.camera.ui.adapters.MyFragmentsPagerAdapter;
 import com.camera.camera.ui.base.BaseFragment;
@@ -15,6 +24,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST_CODE = 200;
     ViewPager container;
     TabLayout tabLayout;
 
@@ -26,22 +36,78 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        if (checkPermission()) {
 
-        container = findViewById(R.id.container);
-        tabLayout = findViewById(R.id.tabLayout);
+            container = findViewById(R.id.container);
+            tabLayout = findViewById(R.id.tabLayout);
 
-        Fragment photoFragment = new Photo();
-        Fragment portretFragment = new Portret();
-        Fragment videoFragment = new Video();
+            Fragment photoFragment = new Photo();
+            Fragment portretFragment = new Portret();
+            Fragment videoFragment = new Video();
 
-        ArrayList<BaseFragment> fragments = new ArrayList<>();
-        fragments.add((BaseFragment) photoFragment);
-        fragments.add((BaseFragment) portretFragment);
-        fragments.add((BaseFragment) videoFragment);
+            ArrayList<BaseFragment> fragments = new ArrayList<>();
+            fragments.add((BaseFragment) photoFragment);
+            fragments.add((BaseFragment) portretFragment);
+            fragments.add((BaseFragment) videoFragment);
 
-        MyFragmentsPagerAdapter adapter = new MyFragmentsPagerAdapter(getSupportFragmentManager());
-        adapter.setList(fragments);
-        container.setAdapter(adapter);
-        tabLayout.setupWithViewPager(container);
+            MyFragmentsPagerAdapter adapter = new MyFragmentsPagerAdapter(getSupportFragmentManager());
+            adapter.setList(fragments);
+            container.setAdapter(adapter);
+            tabLayout.setupWithViewPager(container);
+
+        } else {
+            requestPermission();
+        }
+    }
+
+    private boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
+        }
+        return true;
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                                != PackageManager.PERMISSION_GRANTED) {
+                            showMessageOKCancel("You need to allow access permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermission();
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+            .setMessage(message)
+            .setPositiveButton("OK", okListener)
+            .setNegativeButton("Cancel", null)
+            .create()
+            .show();
     }
 }
